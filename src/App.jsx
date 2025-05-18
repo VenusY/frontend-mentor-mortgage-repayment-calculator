d977299014169004c0d073f54f59a683cb0153b3
Venus <venusyip@outlook.com>
2025-05-18 21:49:15 +0100

    Add InputSection and ResultsSection components to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,12 @@
-export default function App() {}
+import './styles/reset.css';
+import InputSection from './components/InputSection';
+import ResultsSection from './components/ResultsSection';
+
+export default function App() {
+  return (
+    <div className='app'>
+      <InputSection />
+      <ResultsSection />
+    </div>
+  );
+}
