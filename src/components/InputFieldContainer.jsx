7020e18db9e711ec3a7f4321e34889f174a4b078
Venus <venusyip@outlook.com>
2025-05-20 23:32:27 +0100

    Accept reversed as a prop in InputFieldContainer component

--- src/components/InputFieldContainer.jsx
+++ src/components/InputFieldContainer.jsx
@@ -1,6 +1,10 @@
-export default function InputFieldContainer({ unit, id }) {
+export default function InputFieldContainer({ unit, id, reversed }) {
   return (
-    <div className='input-field-container'>
+    <div
+      className={`input-field-container ${
+        reversed ? 'input-field-container--reversed' : ''
+      }`}
+    >
       <div className='input-field-container__units-container'>{unit}</div>
       <input
         type='text'
