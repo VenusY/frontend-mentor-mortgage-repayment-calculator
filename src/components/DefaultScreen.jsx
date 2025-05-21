122d54ac2a26751b4fccdb37c6a043981eb81726
Venus <venusyip@outlook.com>
2025-05-21 09:42:46 +0100

    Add markup for default results screen

--- src/components/DefaultScreen.jsx
+++ src/components/DefaultScreen.jsx
@@ -1,1 +1,16 @@
-export default function DefaultScreen() {}
+export default function DefaultScreen() {
+  return (
+    <section className='default-screen'>
+      <img
+        src={require('../assets/images/illustration-empty.svg')}
+        alt='Sheet and calculator'
+        className='default-screen__hero-image'
+      />
+      <h2 className='default-screen__heading'>Results shown here</h2>
+      <p className='default-screen__description'>
+        Complete the form and click "calculate repayments" to see what your
+        monthly repayments would be.
+      </p>
+    </section>
+  );
+}
