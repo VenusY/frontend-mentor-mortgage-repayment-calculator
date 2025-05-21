8306080bd0e52e180f4c91b0eebdfda598c84271
Venus <venusyip@outlook.com>
2025-05-21 10:04:20 +0100

    Add markup for the results screen

--- src/components/ResultsScreen.jsx
+++ src/components/ResultsScreen.jsx
@@ -1,1 +1,28 @@
-export default function ResultsScreen() {}
+export default function ResultsScreen({ monthlyRepayments, total }) {
+  return (
+    <section className='results-screen'>
+      <header className='results-screen__header'>
+        <h2 className='results-screen__heading'>Your results</h2>
+        <p className='results-screen__description'>
+          Your results are shown below based on the information you provided. To
+          adjust the results, edit the form and click "calculate repayments"
+          again.
+        </p>
+      </header>
+
+      <main className='results-screen__main-section'>
+        <section className='monthly-repayments'>
+          <h3 className='monthly-repayments__heading'>
+            Your monthly repayments
+          </h3>
+          <p className='monthly-repayments__result'>{monthlyRepayments}</p>
+        </section>
+
+        <section className='total'>
+          <h3 className='total__heading'>Total you'll repay over the term</h3>
+          <p className='total__result'>{total}</p>
+        </section>
+      </main>
+    </section>
+  );
+}
