ebc12364b279b88c66667987fd150b823a1b0d4d
Venus <venusyip@outlook.com>
2025-05-22 02:57:13 +0100

    Add state variables for inputs and results

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import './styles/reset.css';
+import { useState } from 'react';
 import InputSection from './components/InputSection';
 import ResultsSection from './components/ResultsSection';
 
 export default function App() {
+  const [mortgageAmount, setMortgageAmount] = useState(null);
+  const [mortgageTerm, setMortgageTerm] = useState(null);
+  const [interestRate, setInterestRate] = useState(null);
+  const [mortgageType, setMortgageType] = useState(null);
+  const [monthlyRepayments, setMonthlyRepayments] = useState(null);
+  const [total, setTotal] = useState(null);
+  const [displayResults, setDisplysResults] = useState(false);
+
   return (
     <div className='calculator'>
       <InputSection />
-      <ResultsSection />
+      <ResultsSection displayResults={displayResults} />
     </div>
   );
 }
