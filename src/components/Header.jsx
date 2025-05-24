639af7992a1279b3cb68f25d48e047ac16f504ea
Venus <venusyip@outlook.com>
2025-05-24 11:47:34 +0100

    Extract input section header markup into Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className='input-section__header'>
+      <h1 className='input-section__heading'>Mortgage Calculator</h1>
+      <button className='input-section__clear-button'>Clear All</button>
+    </header>
+  );
+}
--- src/components/InputSection.jsx
+++ src/components/InputSection.jsx
@@ -1,13 +1,10 @@
+import Header from './Header';
 import Form from './Form';
 
 export default function InputSection() {
   return (
     <section className='input-section'>
-      <header className='input-section__header'>
-        <h1 className='input-section__heading'>Mortgage Calculator</h1>
-        <button className='input-section__clear-button'>Clear All</button>
-      </header>
-
+      <Header />
       <Form />
     </section>
   );
