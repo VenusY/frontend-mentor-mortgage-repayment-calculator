fcea070120e7b281de90b093325cb5950c0ba8de
Venus <venusyip@outlook.com>
2025-05-26 23:35:59 +0100

    Add mortgage type radio button functionality

--- src/components/RadioButton.jsx
+++ src/components/RadioButton.jsx
@@ -1,4 +1,13 @@
+import { useContext } from 'react';
+import { CalculatorContext } from '../App';
+
 export default function RadioButton({ label, value }) {
+  const { setMortgageType } = useContext(CalculatorContext);
+
+  function updateMortgageType() {
+    setMortgageType(value);
+  }
+
   return (
     <div className='input-section__radio-button'>
       <label htmlFor={value} className='input-section__radio-label'>
@@ -8,6 +17,7 @@ export default function RadioButton({ label, value }) {
         type='radio'
         name='mortgage-type'
         value={value}
+        onChange={updateMortgageType}
         id={value}
         className='input-section__input-field'
       />
