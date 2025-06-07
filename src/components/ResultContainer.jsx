c03da8b22f71c2b99562f17405c9d8496121f0ce
Venus <venusyip@outlook.com>
2025-06-07 23:44:43 +0100

    Change class of result container heading and add styling

--- src/components/ResultContainer.jsx
+++ src/components/ResultContainer.jsx
@@ -1,7 +1,9 @@
+import '../styles/scss/ResultContainer.scss';
+
 export default function ResultContainer({ category, heading, result }) {
   return (
     <section className={category}>
-      <h3 className={`${category}__heading`}>{heading}</h3>
+      <h3 className='result-container__heading'>{heading}</h3>
       <p className={`${category}__result`}>{result}</p>
     </section>
   );
