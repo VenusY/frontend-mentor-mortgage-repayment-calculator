22774ee82488e0fa98f33c6ea26095873e8ac974
Venus <venusyip@outlook.com>
2025-06-12 10:04:24 +0100

    Add input field validation

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,52 @@
 import '../styles/scss/Form.scss';
+import { useContext } from 'react';
+import { CalculatorContext } from '../App';
 import MortgageAmount from './MortgageAmount';
 import MortgageTermAndInterestRate from './MortgageTermAndInterestRate';
 import MortgageType from './MortgageType';
 import CalculateButton from './CalculateButton';
 
 export default function Form() {
+  const {
+    mortgageAmount,
+    mortgageTerm,
+    interestRate,
+    mortgageType,
+    setValid,
+    setDisplayError,
+  } = useContext(CalculatorContext);
+
+  function validateInputs() {
+    const amountRegex = /^(\d+|\.\d{1,2}|\d+\.\d{1,2})$/;
+    const termRegex = /^\d+$/;
+    const interestRateRegex = /^(\d+|\.\d+|\d+\.\d+)$/;
+
+    setValid({
+      mortgageAmount: mortgageAmount.match(amountRegex) ? true : false,
+      mortgageTerm: mortgageTerm.match(termRegex) ? true : false,
+      interestRate: interestRate.match(interestRateRegex) ? true : false,
+      mortgageType: mortgageType ? true : false,
+    });
+
+    setDisplayError({
+      mortgageAmount: !mortgageAmount ? true : false,
+      mortgageTerm: !mortgageTerm ? true : false,
+      interestRate: !interestRate ? true : false,
+      mortgageType: !mortgageType ? true : false,
+    });
+  }
+
+  function calculate() {}
+
   return (
-    <form className='form'>
+    <form
+      className='form'
+      onSubmit={(e) => {
+        e.preventDefault();
+        validateInputs();
+        calculate();
+      }}
+    >
       <div className='form__input-fields'>
         <MortgageAmount />
         <MortgageTermAndInterestRate />
